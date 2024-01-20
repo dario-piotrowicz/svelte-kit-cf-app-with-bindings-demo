0c0258856c93367fc82b09217f03706165e5b2c3
Dario Piotrowicz <dario@cloudflare.com>
2024-01-20 15:39:25 +0000

    use local version of cloudflare adapter (with the auto populated platform)

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import { getBindingsProxy } from "wrangler";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,9 +12,6 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({ nodeCompat: true }),
-    devPlatform: {
-      env: (await getBindingsProxy()).bindings,
-    },
   },
 };
 
